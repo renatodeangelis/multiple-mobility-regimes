a3b671f85e381be7cc4fed091f4221fb67eb2cbb
Renato de Angelis <98622953+renatodeangelis@users.noreply.github.com>
2025-09-19 13:00:00 -0500

    Update convergence_analysis.R

--- code/convergence_analysis.R
+++ code/convergence_analysis.R
@@ -8,55 +8,70 @@ gdim = read_csv("https://www.dropbox.com/scl/fi/rfhjr8vsozbm3tf9b0hl1/GDIM_2023_
 
 # Helper to compute indicator column from measure choice
 .add_indicator = function(df, measure){
+ 
   key = tolower(gsub("[^a-z0-9]", "", measure))
-
-  if (key %in% c("1beta","1cor","mu050","bhq4",
-                 "mu050randomtiebreak","bhq4randomtiebreak")) {
-    if (key == "1beta") {
-      df$val = 1 - df$BETA
-    } else if (key == "1cor") {
-      df$val = 1 - df$COR
-    } else if (key %in% c("mu050","mu050randomtiebreak")) {
-      df$val = df$MU050_randomtiebreak
-    } else if (key %in% c("bhq4","bhq4randomtiebreak")) {
-      df$val = df$BHQ4_randomtiebreak
-    }
-    return(df[, c("country","cohort","val")])
+  
+  if (key == "1beta") {
+    df = df |> mutate(val = 1 - .data[["BETA"]])
+  } else if (key == "1cor") {
+    df = df |> mutate(val = 1 - .data[["COR"]])
+  } else if (key == "mu050") {
+    df = df |> mutate(val = .data[["MU050_randomtiebreak"]])
+  } else if (key == "bhq4") {
+    df = df |> mutate(val = .data[["BHQ4_randomtiebreak"]])
+  } else if (key == "ahmp") {
+    df = df |> mutate(val = .data[["CAT_ISCED0"]])
+  } else if (key == "mix") {
+    df = df |> mutate(val = .data[["MIX"]])
   } else {
-    stop("`measure` not recognized. Try one of: '1-beta','1-cor','MU050','BHQ4'.")
+    stop("`measure` not recognized. Use one of: '1-beta', '1-cor', 'mu050', 'bhq4', 'ahmp', 'mix'.")
   }
+  
+  df |> select(country, cohort, val)
 }
 
 
 # Function to build balanced panel with relevant indicators
 make_panel = function(data, parent = "max", child = "all", measure = "1-beta"){
-  df = gdim |> filter(.data$parent == "parent", .data$child == "child") |>
-  .add_indicator("measure") |>
-  select(country, cohort, val)
-
+  
+  df = data |> filter(.data$parent == !!parent, .data$child == !!child) |>
+    .add_indicator(measure)
+  
   wide = df |>
-  pivot_wider(names_from = "cohort", values_from = "val") |>
-  arrange(country) |>
-  as.data.frame()
-
+    pivot_wider(names_from = "cohort", values_from = "val") |>
+    arrange(country) |>
+    as.data.frame()
+  
   wide
-  }
+}
 
 # Function to run Phillips-Sul tests
-run_ps = function(panel_df, dataCols, time_trim = 0.2, HACmethod = "FQSB", cstar = 0, refCol = NULL) {
+run_ps = function(data,
+                  parent = "max",
+                  child = "all",
+                  measure = "1-beta",
+                  dataCols,
+                  time_trim = 0.3,
+                  HACmethod = "FQSB",
+                  cstar = 0,
+                  refCol = NULL) {
+  
+  panel_df = make_panel(data, parent, child, measure)
+  
   stopifnot(is.data.frame(panel_df))
   if(is.null(refCol)) {
     refCol = max(dataCols)
-    }
-
+  }
+  
   H = computeH(panel_df[, dataCols], quantity = "H")
   global = estimateMod(H, time_trim = time_trim, HACmethod = HACmethod)
-
+  
   clubs = findClubs(panel_df, dataCols = dataCols, unit_names = 1, refCol = refCol,
                     time_trim = time_trim, cstar = cstar, HACmethod = HACmethod)
-
+  
   list(H = H, global = global, clubs = clubs)
-  }
+}
+
 
 # Restrict to countries with observations for 1940-1980 cohorts
 keep_1940 = gdim |> group_by(country) |> count() |> filter(n == 60) |> pull(country)
@@ -75,7 +90,13 @@ x_1940 = gdim_1940 |>
   as.data.frame()
 
 H = computeH(x_1940[,-1], quantity = "H")
-round(estimateMod(H, time_trim=1/3, HACmethod = "FQSB"), 3)
+round(estimateMod(H, time_trim = 0.3, HACmethod = "FQSB"), 3)
 
 clubs = findClubs(x_1940, dataCols = 2:6, unit_names = 1, refCol = 6,
-                  time_trim = 0.2, cstar = 0, HACmethod = 'FQSB')  
+                  time_trim = 0.2, cstar = -1.65, HACmethod = 'FQSB')
+
+
+
+
+
+
