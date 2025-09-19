eb855f6251663949c82d70df1c004cf4eb456c8f
Renato de Angelis <98622953+renatodeangelis@users.noreply.github.com>
2025-09-19 13:00:57 -0500

    Update convergence_analysis.R

--- code/convergence_analysis.R
+++ code/convergence_analysis.R
@@ -77,26 +77,5 @@ run_ps = function(data,
 keep_1940 = gdim |> group_by(country) |> count() |> filter(n == 60) |> pull(country)
 gdim_1940 = gdim |> filter(country %in% keep_1940)
 
-# Restrict to countries with observations for 1950-1980 cohorts
-keep_1950 = gdim |> group_by(country) |> count() |> filter(n >= 48) |> pull(country)
-gdim_1950 = gdim |> filter(country %in% keep_1950)
-
-x_1940 = gdim_1940 |>
-  filter(parent == "max", child == "all") |>
-  mutate(ige = 1 - BETA) |>
-  select(country, cohort, ige) |>
-  pivot_wider(names_from = "cohort", values_from = "ige") |>
-  arrange(country) |>
-  as.data.frame()
-
-H = computeH(x_1940[,-1], quantity = "H")
-round(estimateMod(H, time_trim = 0.3, HACmethod = "FQSB"), 3)
-
-clubs = findClubs(x_1940, dataCols = 2:6, unit_names = 1, refCol = 6,
-                  time_trim = 0.2, cstar = -1.65, HACmethod = 'FQSB')
-
-
-
-
 
 
